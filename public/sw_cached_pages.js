19575a804389fb25854ccd4c58b318957b7afa59
Michel-Alexandre Belanger <michel-alexandre.belanger@tritondigital.com>
2019-04-30 09:15:33 -0400

    PDEV-8953 : update

--- public/sw_cached_pages.js
+++ public/sw_cached_pages.js
@@ -1,18 +1,23 @@
-const cacheName = 'v1';
+const cacheName = 'v3';
 
 const cacheAssets = [
-  'index.html',
+  'toto.js',
   'about.html'
 ];
 
 // Call install Event
 self.addEventListener('install', (event) => {
   console.log(`Service worker Installed`);
+  // wait that sw is ready.
   event.waitUntil(
     caches
       .open(cacheName)
-      .then((cache)=>{
+      .then((cache) => {
         console.log('SW caching files');
+        cache.addAll(cacheAssets);
+      })
+      .then(()=>{
+        self.skipWaiting();
       })
   );
 });
@@ -20,4 +25,18 @@ self.addEventListener('install', (event) => {
 // Call Activate Event
 self.addEventListener('activate', (event) => {
   console.log(`Service worker Activated`)
+  // Remove unwanted caches
+  event.waitUntil(
+    caches.keys()
+      .then((cacheNames)=>{
+        return Promise.all(
+          cacheNames.map((cache)=>{
+            if(cache !== cacheName) {
+              console.log('SW : clearing olad cache');
+              return caches.delete(cache);
+            }
+          })
+        )
+      })
+  )
 });
